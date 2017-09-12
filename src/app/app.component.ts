c84ddc0d2fd333529800f34978d5757021b4e643
DevYmA <ymamarathunga@gmail.com>
2017-09-12 12:20:30 +0530

    Updated dashboard with crud operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,18 @@
-import { Component } from '@angular/core';
-
-export class Hero{
-  id:number;
-  name:string;
-}
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector:'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div>
-      <label>id:</label>{{hero.id}}
-    </div>
-    <div>
-  <label>name: </label>
-  <input [(ngModel)]="hero.name" name="name" placeholder="name">
-</div>
-  `,
+   <h1>{{title}}</h1>
+   <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+   </nav>
+
+   <router-outlet></router-outlet>
+ `,
+ styleUrls:['./app.component.css']
 })
-export class AppComponent  {
-  title = 'Tour of Hero';
-  hero:Hero = {
-    id : 1,
-    name : 'Windstrom'
-  }
+export class AppComponent{
+  title = 'Tour of Heroes';
 }
